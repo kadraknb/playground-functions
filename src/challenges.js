88723ff1a8acf9353d2122f529986d5fe49a499f
kadraknb <kadrak02@hotmail.com>
2022-04-19 12:08:01 -0300

    Requisitos 8,9

--- src/challenges.js
+++ src/challenges.js
@@ -23,8 +23,8 @@ function splitSentence(a) {
 /* npm test  */
 // Desafio 4
 function concatName(arr) {
-  let ult = arr.length -1;
-  let ultPri = arr[ult]+ ", " + arr[0];
+  let ult = arr.length - 1;
+  let ultPri = `${arr[ult]}, ${arr[0]}`;
   return ultPri;
 }
 
@@ -57,22 +57,53 @@ function catAndMouse(mouse, cat1, cat2) {
   let ca2 = Math.abs(cat2 - mouse);
   if (ca1 > ca2) {
     return 'cat2';
-  } if (ca2 > ca1) {
+  }
+  if (ca2 > ca1) {
     return 'cat1';
   }
   return 'os gatos trombam e o rato foge';
 }
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(nun) {
+  let fizz = [];
+  for (let i = 0; i < nun.length; i += 1) {
+    if (nun[i] % 5 == 0 && nun[i] % 3 == 0) {
+      fizz.push('fizzBuzz');
+    } else if (nun[i] % 3 == 0) {
+      fizz.push('fizz');
+    } else if (nun[i] % 5 == 0) {
+      fizz.push('buzz');
+    } else {
+      fizz.push('bug!');
+    }
+  } return fizz;
 }
-
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(aa) {
+  let fArr = aa.split('');
+  let vogais = ['a', 'e', 'i', 'o', 'u'];
+  for (let i in fArr) {
+    for (let i2 = 0; i2 < vogais.length; i2 += 1) {
+      if (fArr[i] === vogais[i2]) {
+        fArr[i] = i2 + 1;
+      }
+    }
+  }
+  let aaa = fArr.join('');
+  return aaa;
 }
-function decode() {
-  // seu código aqui
+function decode(aa) {
+  let fArr = aa.split('');
+  let vogais = ['a', 'e', 'i', 'o', 'u'];
+  for (let i in fArr) {
+    for (let i2 = 0; i2 < vogais.length; i2 += 1) {
+      if (fArr[i] == i2 + 1) {
+        fArr[i] = vogais[i2];
+      }
+    }
+  }
+  let aaa = fArr.join('');
+  return aaa;
 }
 
 // Desafio 10
